8c64af68bee2486063b83992a57b26dedda5c2c0
Rimarok <romaric.ruga@gmail.com>
2022-12-01 00:10:25 +0100

    package index : export 2 more components

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
 import DefaultTemplate from "./src/templates/default.template";
 import type { DefaultTemplateContext } from "./src/templates/default.template";
 import type { ExtendedBlockObjectResponse } from "./src/types/ExtendedBlockObjectResponse";
+import RichTextRenderer from "./src/components/RichTextRenderer";
+import PureBlocksRenderer from "./src/components/PureBlocksRenderer";
 
-export { DefaultTemplate, DefaultTemplateContext, ExtendedBlockObjectResponse };
+export {
+  DefaultTemplate,
+  DefaultTemplateContext,
+  ExtendedBlockObjectResponse,
+  RichTextRenderer,
+  PureBlocksRenderer,
+};

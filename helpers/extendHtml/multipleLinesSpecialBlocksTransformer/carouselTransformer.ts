291dec53476dc7af0be30aa1f625c49e815b41dd
Rimarok <romaric.ruga@mailo.fr>
2025-04-22 21:16:58 +0200

    v2.2.8 : fix : export videoTransformer + rm console.log in carouselTransformer

--- helpers/extendHtml/multipleLinesSpecialBlocksTransformer/carouselTransformer.ts
+++ helpers/extendHtml/multipleLinesSpecialBlocksTransformer/carouselTransformer.ts
@@ -1,5 +1,4 @@
 export function carouselTransformer(html: string) {
-  console.log(html);
   // Remove paragraph tags around image tags
   const imagePattern = /<p>\s*(<img[^>]*\/>)\s*<\/p>/g;
   html = html.replace(imagePattern, (match, imgTag) => imgTag);
